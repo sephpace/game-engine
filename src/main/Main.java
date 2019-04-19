928d83187e899f054d071bf31ef149caa51db351
Seph Pace <sephpace@gmail.com>
2019-04-18 22:58:14 -0600

    Add Main.java.

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+/**
+ * Main class.  Starts all other parts of the program.
+ *
+ * @author  Seph Pace
+ * @version 1.0
+ */
+public class Main {
+
+    /**
+     * Main method.
+     *
+     * @param args  Arguments from the command line: none expected
+     */
+    public static void main(String[] args) {
+
+    }
+}
