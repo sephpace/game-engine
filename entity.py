Fix camera rotation and add correct camera movement.

entity.py
@@ -14,12 +14,13 @@ class Entity(ABC):
     An entity is an object that can move.
 
     Attributes:
-    __position (ndarray): The position of the entity in 3D space.
-    __velocity (ndarray): The velocity of the entity in 3D space.
-    __rotation (ndarray): A vector containing angles of rotation for the x, y, and z axes.
+    position (ndarray): The position of the entity in 3D space.
+    velocity (ndarray): The velocity of the entity in 3D space.
+    rotation (ndarray): A vector containing angles of rotation for the x, y, and z axes.
+    speed (float):      The speed of the entity's movement.
     """
 
-    def __init__(self, position=np.zeros(3), velocity=np.zeros(3), rotation=np.zeros(3)):
+    def __init__(self, position=np.zeros(3), velocity=np.zeros(3), rotation=np.zeros(3), speed=0.1):
         """
         Constructor.
 
@@ -27,10 +28,12 @@ class Entity(ABC):
         position (ndarray): The position of the entity in 3D space.
         velocity (ndarray): The velocity of the entity in 3D space.
         rotation (ndarray): A vector containing angles of rotation for the x, y, and z axes.
+        speed (float):      The speed of the entity's movement.
         """
         self.position = position
         self.velocity = velocity
         self.rotation = rotation
+        self.speed = speed
 
     def update(self, delta):
         """
@@ -46,7 +49,7 @@ class Camera(Entity):
     Used as the origin point in ray marching.
     """
 
-    def __init__(self, position=np.zeros(3), velocity=np.zeros(3), rotation=np.zeros(3), direction=np.zeros(2), zoom=ZOOM, width=SCREEN_WIDTH, height=SCREEN_HEIGHT):
+    def __init__(self, position=np.zeros(3), velocity=np.zeros(3), rotation=np.zeros(3), zoom=ZOOM, width=SCREEN_WIDTH, height=SCREEN_HEIGHT):
         """
         Constructor.
 
@@ -54,13 +57,11 @@ class Camera(Entity):
         position (ndarray):  The position of the entity in 3D space.
         velocity (ndarray):  The velocity of the entity in 3D space.
         rotation (ndarray):  A vector containing angles (radians) of rotation for the x, y, and z axes.
-        direction (ndarray): The rotation and elevation angles denoting the direction the camera is facing.
         zoom (float):        The zoom of the camera.
         width (float):       The width of the screen in 3D space.
         height (float):      The height of the screen in 3D space.
         """
         super(Camera, self).__init__(position, velocity, rotation)
-        self.direction = direction
         self.zoom = zoom
         self.width = width
         self.height = height
@@ -80,15 +81,37 @@ class Camera(Entity):
                                           [+width / 2, -height / 2, self.zoom],
                                           [+width / 2, +height / 2, self.zoom]])
 
+        # Movement booleans
+        self.move_forward = False
+        self.move_left = False
+        self.move_backward = False
+        self.move_right = False
+        self.move_up = False
+        self.move_down = False
+
     def update(self, delta):
         """
         Code to be called every frame.
         """
         super(Camera, self).update(delta)
-        theta, phi = self.direction
-        self.rotation = np.array([np.cos(theta) * np.sin(phi),
-                                  np.sin(theta) * np.sin(phi),
-                                  -np.cos(phi)])
         self.screen['camera'] = self.position
         self.screen['rotation'] = self.rotation
         self.screen.draw(gl.GL_TRIANGLE_STRIP)
+
+        # Movement
+        if self.move_forward:
+            rot = self.rotation[1] + np.pi / 2
+            self.position += np.array([np.cos(rot), 0.0, np.sin(rot)]) * self.speed
+        if self.move_left:
+            rot = self.rotation[1] - np.pi
+            self.position += np.array([np.cos(rot), 0.0, np.sin(rot)]) * self.speed
+        if self.move_backward:
+            rot = self.rotation[1] - np.pi / 2
+            self.position += np.array([np.cos(rot), 0.0, np.sin(rot)]) * self.speed
+        if self.move_right:
+            rot = self.rotation[1]
+            self.position += np.array([np.cos(rot), 0.0, np.sin(rot)]) * self.speed
+        if self.move_up:
+            self.position += np.array([0.0, self.speed, 0.0])
+        if self.move_down:
+            self.position -= np.array([0.0, self.speed, 0.0])

main.py
@@ -1,14 +1,15 @@
 
-from glumpy import app, gl
-from pyglet.window import key
+from glumpy import app
+from glumpy.app.window import key
 import numpy as np
 
 from entity import Camera
-from constants import WINDOW_WIDTH, WINDOW_HEIGHT, SPEED
+from constants import WINDOW_WIDTH, WINDOW_HEIGHT
 
+app.use('pyglet')
 window = app.Window(WINDOW_WIDTH, WINDOW_HEIGHT)
 
-camera = Camera(position=np.array([0.0, 0.0, -5.0]))
+camera = Camera(position=np.array([0.0, 0.0, 0.0]))
 
 
 @window.event
@@ -19,40 +20,44 @@ def on_draw(dt):
 
 @window.event
 def on_key_press(symbol, modifiers):
+    if symbol == key.ESCAPE:
+        window.close()
+        exit()
+
     if symbol == key.W:
-        camera.velocity += np.array([0.0, 0.0, +SPEED])  # Forward
+        camera.move_forward = True
     elif symbol == key.A:
-        camera.velocity += np.array([-SPEED, 0.0, 0.0])  # Left
+        camera.move_left = True
     elif symbol == key.S:
-        camera.velocity += np.array([0.0, 0.0, -SPEED])  # Backward
+        camera.move_backward = True
     elif symbol == key.D:
-        camera.velocity += np.array([+SPEED, 0.0, 0.0])  # Right
+        camera.move_right = True
     elif symbol == key.SPACE:
-        camera.velocity += np.array([0.0, +SPEED, 0.0])  # Up
+        camera.move_up = True
     elif symbol == key.LSHIFT:
-        camera.velocity += np.array([0.0, -SPEED, 0.0])  # Down
+        camera.move_down = True
 
 
 @window.event
 def on_key_release(symbol, modifiers):
     if symbol == key.W:
-        camera.velocity += np.array([0.0, 0.0, -SPEED])  # Forward
+        camera.move_forward = False
     elif symbol == key.A:
-        camera.velocity += np.array([+SPEED, 0.0, 0.0])  # Left
+        camera.move_left = False
     elif symbol == key.S:
-        camera.velocity += np.array([0.0, 0.0, +SPEED])  # Backward
+        camera.move_backward = False
     elif symbol == key.D:
-        camera.velocity += np.array([-SPEED, 0.0, 0.0])  # Right
+        camera.move_right = False
     elif symbol == key.SPACE:
-        camera.velocity += np.array([0.0, -SPEED, 0.0])  # Up
+        camera.move_up = False
     elif symbol == key.LSHIFT:
-        camera.velocity += np.array([0.0, +SPEED, 0.0])  # Down
+        camera.move_down = False
 
 
 @window.event
 def on_mouse_motion(x, y, dx, dy):
-    camera.direction += np.array([dx, -dy]) * np.pi / 500
-    # TODO: Find a way to stop the cursor from moving off the screen
+    camera.rotation -= np.array([dy, dx, 0]) * np.pi / 500
+    camera.rotation[1] = np.clip(camera.rotation[1], -np.pi, np.pi)  # TODO: Fix this so it actually clamps the values correctly
 
 
 app.run()